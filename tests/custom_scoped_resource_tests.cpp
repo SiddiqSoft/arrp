a250e9070297eea534145d4b6959efff2c966ea1
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2026-07-31 21:56:30 -0700

    tweak the output of rht result

--- tests/custom_scoped_resource_tests.cpp
+++ tests/custom_scoped_resource_tests.cpp
@@ -166,7 +166,7 @@ TEST(custom_scoped_resource, write_to_file)
 
         // Verify file contents
         FILE* verify_file = std::fopen(temp_file.c_str(), "r");
-        ASSERT_NE(nullptr, verify_file);
+        ASSERT_FALSE(verify_file==NULL);
 
         char buffer[100] = {0};
         std::fread(buffer, 1, sizeof(buffer), verify_file);
